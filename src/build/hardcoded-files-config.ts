055db10ed8ce0368a22d2c25d601da8d6130c011
flawedworld <flawedworld@flawed.world>
2025-06-17 00:40:51 +0000

    current firmware

--- src/build/hardcoded-files-config.ts
+++ src/build/hardcoded-files-config.ts
@@ -1,176 +1,51 @@
 export const android16filesforpixels: Record<string, string[]> = {
     "tegu": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "comet": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "komodo": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "caiman": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "tokay": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "akita": [
+        'vendor/firmware/bcmdhd_clm.blob_4383_a', // WiFi firmware
+        'vendor/firmware/bcmdhd_clm.blob_MMW', // WiFi firmware
+        'vendor/firmware/bcmdhd_clm.blob_NA',  // WiFi firmware // TODO: This is part of the WiFi firmware but is _only_ on 16, so we need to get adevtool to ship this also
         'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
         'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
         'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
+        'vendor/firmware/fw_bcmdhd.bin_4383_a3', // WiFi firmware
         'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
+        'vendor/firmware/fw_bcmdhd.map_4383_a3', // WiFi firmware
+        'vendor/firmware/google/edgetpu-rio.fw', // TPU
+        'vendor/firmware/google/gxp-callisto.fw', // TPU
+        'vendor/firmware/gxp_callisto_fw_core0', // TPU
+        'vendor/firmware/gxp_callisto_fw_core1', // TPU
+        'vendor/firmware/gxp_callisto_fw_core2', // TPU
     ],
     "husky": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "shiba": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "felix": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "tangorpro": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "lynx": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "cheetah": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "panther": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "bluejay": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "raven": [
-        'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
-        'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
-        'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
-        'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
-        'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
     ],
     "oriole": [
         'vendor/firmware/brcm/BCM.hcd', // Bluetooth firmware
         'vendor/firmware/brcm/BTFW_B.hcd', // Bluetooth firmware
-        'vendor/firmware/carrierconfig/build.info', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.db', // Modem configs
-        'vendor/firmware/carrierconfig/cfg.sha2', // Modem configs
-        'vendor/firmware/carrierconfig/release-label', // Modem configs
         'vendor/firmware/fw_bcmdhd.bin', // WiFi firmware
         'vendor/firmware/fw_bcmdhd.map', // WiFi firmware
         'vendor/firmware/google/edgetpu-abrolhos.fw', // TPU firmware
