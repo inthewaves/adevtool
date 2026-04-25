9a69c6473705e840ed4abe98b9d4a1ed9776b71d
inthewaves <inthewaves@pm.me>
2026-04-24 22:20:18 -0700

    proto-ts: comment out missing apk_parser_config.proto update
    This appears to be missing in frameworks/base, likely old code

--- src/proto-ts/update.sh
+++ src/proto-ts/update.sh
@@ -5,10 +5,10 @@ set -o errexit -o nounset -o pipefail
 protoc --plugin=vendor/adevtool/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out vendor/adevtool/src/proto-ts \
     build/make/tools/aconfig/aconfig_protos/protos/aconfig.proto \
     build/soong/linkerconfig/proto/linker_config.proto \
-    frameworks/base/proto/src/apk_parser_config.proto \
     frameworks/base/tools/aapt2/BriefPackageInfo.proto \
     packages/apps/CarrierConfig2/src/com/google/carrier/carrier_{settings,list}.proto \
     packages/modules/common/proto/classpaths.proto
+    #frameworks/base/proto/src/apk_parser_config.proto
 
 protoc --plugin=vendor/adevtool/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out vendor/adevtool/src/proto-ts \
     vendor/adevtool/assets/request.proto
