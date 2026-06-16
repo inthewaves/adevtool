f040570872007812577f5b91d2edbf20582a1cbf
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-06-16 21:10:55 +0000

    add support for SKU-specific sysconfig subdirs

--- src/processor/sysconfig.ts
+++ src/processor/sysconfig.ts
@@ -5,7 +5,7 @@ import { VendorDirectories } from '../blobs/build'
 import { appendPartitionProps, serializeBlueprint, SoongModule } from '../build/soong'
 import { DeviceConfig, getExcludedPackagesMinusBasePackages, getPackagePermsConfig } from '../config/device'
 import { SystemState } from '../config/system-state'
-import { assertDefined } from '../util/data'
+import { assertDefined, updateMultiMap } from '../util/data'
 import { FilterResult } from '../util/exact-filter'
 import { isDirectory } from '../util/fs'
 import { log } from '../util/log'
@@ -89,6 +89,16 @@ export async function getSysconfigXmlFiles(
   return files
 }
 
+interface PartitionSysconfig {
+  partition: Partition
+  sysconfigDirs: SysconfigDir[]
+}
+
+interface SysconfigDir {
+  dirPath: string
+  filePaths: string[]
+}
+
 export async function processSysconfig(
   deviceConfig: DeviceConfig,
   pathResolver: PathResolver,
@@ -105,154 +115,174 @@ export async function processSysconfig(
 
   let partJobs = Array.from(ALL_SYS_PARTITIONS).map(async partition => {
     let dirJobs = SYSCONFIG_DIR_NAMES.map(async sysconfigDirName => {
-      let sysconfigDirRelPath = path.join('etc', sysconfigDirName)
-      let sysconfigDirPath = pathResolver.resolve(partition, sysconfigDirRelPath)
-      if (!(await isDirectory(sysconfigDirPath))) {
+      let partSysconfigDirRelPath = path.join('etc', sysconfigDirName)
+      let partSysconfigDirPath = pathResolver.resolve(partition, partSysconfigDirRelPath)
+      if (!(await isDirectory(partSysconfigDirPath))) {
         return null
       }
-      let dstDirPath = path.join(rootOutDir, partition, sysconfigDirName)
+      let sysconfigSubDirs = new Map<string, string[]>()
+      for (let de of await fs.readdir(partSysconfigDirPath, { withFileTypes: true, recursive: true })) {
+        if (de.isDirectory()) {
+          continue
+        }
+        assert(de.isFile(), de.name)
+        updateMultiMap(sysconfigSubDirs, de.parentPath, de.name)
+      }
 
-      let fileJobs = (await fs.readdir(sysconfigDirPath, { withFileTypes: true })).map(async dirEntry => {
-        assert(dirEntry.isFile())
+      let sysconfigSubDirJobs: Promise<SysconfigDir>[] = Array.from(sysconfigSubDirs.entries()).map(
+        async ([sysConfigDirPath, sysConfigNames]) => {
+          let dstDirPath = path.join(
+            rootOutDir,
+            partition,
+            sysconfigDirName,
+            path.relative(partSysconfigDirPath, sysConfigDirPath),
+          )
+          let fileJobs = sysConfigNames.map(async sysConfigFileName => {
+            let xmlFilePath = path.join(sysConfigDirPath, sysConfigFileName)
 
-        let xmlFilePath = pathResolver.resolve(partition, path.join(sysconfigDirRelPath, dirEntry.name))
-        let cmd = {
-          xmlFilePath,
-          allowedRootElementNames: ['config', 'permissions', 'exceptions'],
-          dstDirPath,
-          dstFileName: dirEntry.name,
-          filterSpec: {
-            preprocess(entry) {
-              let attrs = entry[':@']
-              if (attrs === undefined) {
-                return null
-              }
-              const packageAttr = '@_package'
-              let pkgName = (attrs as Record<string, string>)[packageAttr]
-              if (pkgName === undefined) {
-                return null
-              }
+            let cmd = {
+              xmlFilePath,
+              allowedRootElementNames: ['config', 'permissions', 'exceptions'],
+              dstDirPath,
+              dstFileName: sysConfigFileName,
+              filterSpec: {
+                preprocess(entry) {
+                  let attrs = entry[':@']
+                  if (attrs === undefined) {
+                    return null
+                  }
+                  const packageAttr = '@_package'
+                  let pkgName = (attrs as Record<string, string>)[packageAttr]
+                  if (pkgName === undefined) {
+                    return null
+                  }
 
-              let mappedPkgName = apkProcResult.packageNameMapping.get(pkgName)
-              if (mappedPkgName === undefined) {
-                return null
-              }
-              let clonedEntry = structuredClone(entry)
-              let clonedAttrs = clonedEntry[':@'] as Record<string, string>
-              clonedAttrs[packageAttr] = mappedPkgName
-              return clonedEntry
-            },
-            process: (_, entry) => {
-              let attrs = entry[':@']
-              if (attrs === undefined) {
-                return FilterResult.UNKNOWN_ENTRY
-              }
-              const packageAttr = '@_package'
-              let pkgName = (attrs as Record<string, string>)[packageAttr]
-              if (pkgName === undefined) {
-                return FilterResult.UNKNOWN_ENTRY
-              }
-
-              if (excludedPackages.has(pkgName)) {
-                return FilterResult.EXCLUDE
-              }
-
-              if (!apkProcResult.allPackageNames.has(pkgName)) {
-                return FilterResult.EXCLUDE
-              }
-
-              let inclusionConfig = deviceConfig.package_inclusions[pkgName]
-
-              let elementName = Object.keys(entry)[0]
-              switch (elementName) {
-                case 'exception':
-                case 'privapp-permissions': {
-                  if (inclusionConfig === undefined) {
+                  let mappedPkgName = apkProcResult.packageNameMapping.get(pkgName)
+                  if (mappedPkgName === undefined) {
+                    return null
+                  }
+                  let clonedEntry = structuredClone(entry)
+                  let clonedAttrs = clonedEntry[':@'] as Record<string, string>
+                  clonedAttrs[packageAttr] = mappedPkgName
+                  return clonedEntry
+                },
+                process: (_, entry) => {
+                  let attrs = entry[':@']
+                  if (attrs === undefined) {
                     return FilterResult.UNKNOWN_ENTRY
                   }
-                  let ppc = getPackagePermsConfig(inclusionConfig)
-                  let unknownPerms: string[] = []
-                  let origChildren = entry[elementName] as Record<string, unknown>[]
-                  let filteredChildren = origChildren.filter(child => {
-                    let keys = Object.keys(child)
-                    assert(keys.length === 2)
-                    assert(keys[0] === 'permission')
-                    assert(keys[1] === ':@')
-                    let childAttrs = child[':@'] as Record<string, string>
-                    let permName = assertDefined(childAttrs['@_name'])
-                    if (ppc.removePerms.has(permName)) {
-                      return false
-                    }
-                    if (ppc.pregrantablePerms.has(permName)) {
-                      return true
-                    }
-                    unknownPerms.push(permName)
-                    return true
-                  })
-                  if (unknownPerms.length > 0) {
-                    unknownPerms.sort()
-                    log(pkgName + ': included unknown sysconfig ' + elementName + ' entries:')
-                    for (let p of unknownPerms) {
-                      log('      - ' + p)
-                    }
-                  }
-                  if (filteredChildren.length === 0) {
-                    return FilterResult.EXCLUDE
-                  }
-                  if (origChildren.length === filteredChildren.length) {
-                    return FilterResult.INCLUDE
-                  }
-                  let res = structuredClone(entry)
-                  res[elementName] = structuredClone(filteredChildren)
-                  return res
-                }
-                default: {
-                  if (inclusionConfig === undefined) {
+                  const packageAttr = '@_package'
+                  let pkgName = (attrs as Record<string, string>)[packageAttr]
+                  if (pkgName === undefined) {
                     return FilterResult.UNKNOWN_ENTRY
                   }
-                  let xmlStr = stringifyXml([entry])
-                  if (inclusionConfig.sysconfig_inclusions?.includes(xmlStr)) {
-                    return FilterResult.INCLUDE
-                  }
-                  if (inclusionConfig.sysconfig_exclusions?.includes(xmlStr)) {
+
+                  if (excludedPackages.has(pkgName)) {
                     return FilterResult.EXCLUDE
                   }
-                  return FilterResult.UNKNOWN_ENTRY
-                }
-              }
-            },
-            exclusions,
-            inclusions,
-            unknownEntriesMessagePrefix: 'included unknown sysconfigs from ' + xmlFilePath + ':',
-            yamlPath: [''],
-          },
-        } as ProcessXmlCmd
 
-        return await processXml(cmd)
-      })
-      let paths2 = await Promise.all(fileJobs)
-      let paths: string[] = []
-      paths2.forEach(e => paths.push(...e))
-      return [dstDirPath, paths]
+                  if (!apkProcResult.allPackageNames.has(pkgName)) {
+                    return FilterResult.EXCLUDE
+                  }
+
+                  let inclusionConfig = deviceConfig.package_inclusions[pkgName]
+
+                  let elementName = Object.keys(entry)[0]
+                  switch (elementName) {
+                    case 'exception':
+                    case 'privapp-permissions': {
+                      if (inclusionConfig === undefined) {
+                        return FilterResult.UNKNOWN_ENTRY
+                      }
+                      let ppc = getPackagePermsConfig(inclusionConfig)
+                      let unknownPerms: string[] = []
+                      let origChildren = entry[elementName] as Record<string, unknown>[]
+                      let filteredChildren = origChildren.filter(child => {
+                        let keys = Object.keys(child)
+                        assert(keys.length === 2)
+                        assert(keys[0] === 'permission')
+                        assert(keys[1] === ':@')
+                        let childAttrs = child[':@'] as Record<string, string>
+                        let permName = assertDefined(childAttrs['@_name'])
+                        if (ppc.removePerms.has(permName)) {
+                          return false
+                        }
+                        if (ppc.pregrantablePerms.has(permName)) {
+                          return true
+                        }
+                        unknownPerms.push(permName)
+                        return true
+                      })
+                      if (unknownPerms.length > 0) {
+                        unknownPerms.sort()
+                        log(pkgName + ': included unknown sysconfig ' + elementName + ' entries:')
+                        for (let p of unknownPerms) {
+                          log('      - ' + p)
+                        }
+                      }
+                      if (filteredChildren.length === 0) {
+                        return FilterResult.EXCLUDE
+                      }
+                      if (origChildren.length === filteredChildren.length) {
+                        return FilterResult.INCLUDE
+                      }
+                      let res = structuredClone(entry)
+                      res[elementName] = structuredClone(filteredChildren)
+                      return res
+                    }
+                    default: {
+                      if (inclusionConfig === undefined) {
+                        return FilterResult.UNKNOWN_ENTRY
+                      }
+                      let xmlStr = stringifyXml([entry])
+                      if (inclusionConfig.sysconfig_inclusions?.includes(xmlStr)) {
+                        return FilterResult.INCLUDE
+                      }
+                      if (inclusionConfig.sysconfig_exclusions?.includes(xmlStr)) {
+                        return FilterResult.EXCLUDE
+                      }
+                      return FilterResult.UNKNOWN_ENTRY
+                    }
+                  }
+                },
+                exclusions,
+                inclusions,
+                unknownEntriesMessagePrefix: 'included unknown sysconfigs from ' + xmlFilePath + ':',
+                yamlPath: [''],
+              },
+            } as ProcessXmlCmd
+
+            return await processXml(cmd)
+          })
+          return {
+            dirPath: dstDirPath,
+            filePaths: (await Promise.all(fileJobs)).flat(),
+          } as SysconfigDir
+        },
+      )
+      return await Promise.all(sysconfigSubDirJobs)
     })
-    let entries = (await Promise.all(dirJobs)).filter(e => e !== null) as [string, string[]][]
-    return [partition, entries]
+    return {
+      partition: partition,
+      sysconfigDirs: (await Promise.all(dirJobs)).filter(e => e !== null).flat(),
+    } as PartitionSysconfig
   })
-  let entries = (await Promise.all(partJobs)) as [Partition, [string, string[]][]][]
+  let partSysconfigs = await Promise.all(partJobs)
   let soongModules: SoongModule[] = []
   let moduleNames: string[] = []
-  for (let [partition, partEntries] of entries) {
-    for (let [dir, dirEntries] of partEntries) {
-      if (dirEntries.length > 0) {
-        let dirName = path.basename(dir)
-        let moduleName = `adevtool_sysconfig_${partition}_${dirName}`
+  for (let partSysconfig of partSysconfigs) {
+    for (let sysconfigDir of partSysconfig.sysconfigDirs) {
+      if (sysconfigDir.filePaths.length > 0) {
+        let partition = partSysconfig.partition
+        let relDirPath = path.relative(path.join(rootOutDir, partition), sysconfigDir.dirPath)
+        let moduleName = `adevtool_sysconfig_${partition}_${relDirPath.replace('/', '_')}`
 
         let soongModule = {
           _type: 'prebuilt_etc',
           name: moduleName,
-          srcs: [path.relative(rootOutDir, dir) + '/*.xml'],
+          srcs: [path.relative(rootOutDir, sysconfigDir.dirPath) + '/*.xml'],
           owner: deviceConfig.device.vendor,
-          sub_dir: dirName,
+          sub_dir: relDirPath,
         } as SoongModule
         appendPartitionProps(soongModule, partition)
         soongModules.push(soongModule)
